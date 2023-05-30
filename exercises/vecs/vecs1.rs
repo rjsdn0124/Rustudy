f80dd2ad68c564df959759cf61ca005f4e70577c
Gonulim <geonu.lim@bertis.com>
2023-05-30 15:02:01 -0700

    벡터 깨달았다..!

--- exercises/vecs/vecs1.rs
+++ exercises/vecs/vecs1.rs
@@ -4,13 +4,19 @@
 // Make me compile and pass the test!
 // Execute `rustlings hint vecs1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// (a,v1,v1) 하면 v1이 바로 없어지네. 앞에게 호출되면 바로 메모리를 놓아주넹.
+// 배열을 vec으로 하는 법!
+// 1. 하나씩 push해주기.
+// 2. 배열변수.to_vec()해주기.
+// 3. vec![a[0],a[1],a[2]] 매크로에 이런 식으로 넣어주기. vec 매크로는 정해진 규칙으로 넣어줘야지 되기때문에 저런 방식만 된다고 한다. vec![a]는 안됨~!
+// 매크로란 println! 처럼 Rust 내의 내장 함수인듯하다!
 
-fn array_and_vec() -> ([i32; 4], Vec<i32>) {
+fn array_and_vec() -> ([i32; 4], Vec<i32>, Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
-
-    (a, v)
+    let mut v1 = Vec::<i32>::new();// TODO: declare your vector here with the macro for vectors
+    for i in a {v1.push(i);}
+    let v2 = a.to_vec();
+    (a, v1, v2)
 }
 
 #[cfg(test)]
@@ -19,7 +25,8 @@ mod tests {
 
     #[test]
     fn test_array_and_vec_similarity() {
-        let (a, v) = array_and_vec();
-        assert_eq!(a, v[..]);
+        let (a, v1, v2) = array_and_vec();
+        assert_eq!(a, v1[..]);
+        assert_eq!(a, v2[..]);
     }
 }
--- exercises/vecs/vecs2.rs
+++ exercises/vecs/vecs2.rs
@@ -6,13 +6,18 @@
 //
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// vector 순회하기.
+// vector를 iter 형태로 바꿔서 순회할 수 있또록.
+// iter은 무조건 iterator 객체로 변경해줌. 얘네는 무조건 mut인듯!!! 앞의 변수가 mut이면 iter_mut 사용하는거고 그냥이면 그냥 iter
+// 1. iter_mut() 벡터 원소들을 mut로 바꿔주고 해당 주소값에 있던 element를 다시 값을 덮어 씌우는 느낌? 그래서 mut이 필요한듯! 그대로 주소값을 써용.
+// 2. iter().map() 인자를 사용할 때는 |element|형식으로 사용하네요. 여기선 element를 인자로 받으니까 주소값이 초기화되고 다시 return해서 shadowing 같은 느낌으로 사용하는거 같아요.
+// 보다는 map은 |element|를 통해서 주소값인 iter 원소들을 그냥 참조값으로 받아오는 거 같아용! 그래서 mut니까 그냥 그 위에 다시 덮어쓰는거고.
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for element in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        ???
+        *element *= 2;
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -23,7 +28,7 @@ fn vec_map(v: &Vec<i32>) -> Vec<i32> {
     v.iter().map(|element| {
         // TODO: Do the same thing as above - but instead of mutating the
         // Vec, you can just return the new number!
-        ???
+        element * 2
     }).collect()
 }
 
@@ -42,6 +47,7 @@ mod tests {
     #[test]
     fn test_vec_map() {
         let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
+        // v주소값을 넘겨주니까 v를 그대로 쓸 수 있는거지. iter는 새로운 객체를 만드는거니까. v에 덮어쓰지 않아요!!
         let ans = vec_map(&v);
 
         assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
